1743ef85e154caa9676a669e3775709e96a344bd
Colbey Chittenden <colbeyc@phase2online.com>
2020-06-24 11:24:34 -0500

    commenting out and documenting the initial purpose of this file

--- bin/data_upload.sh
+++ bin/data_upload.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-cd public/data/weather/current
-rm current-wind-surface-level-gfs-1.0.json
-curl -s http://anapaapps.com/earth/current-wind-surface-level-gfs-1.0.json > current-wind-surface-level-gfs-1.0.json
-pwd
-ls -a -o
-curl -s http://status.anapaapps.com/update.php --user-agent "heroku earth update"
+##################
+# this file was used when the ftp upload was utilized to update the weather pattern data.
+# The FTP upload has apparently been broken for a while so this is no longer needed
+# This script is ran from heroku scheduler ever hour but since it is not needed, it can be disabled or removed
+# leaving it here just commented out to provide an example of what it was doing in case updates need to happen more often
+# currently updates only happen on pushes to master which triggers a deploy, but this process was so there could be intermittent updates without a deploy
+##################
+#cd public/data/weather/current
+#rm current-wind-surface-level-gfs-1.0.json
+#curl -s http://anapaapps.com/earth/current-wind-surface-level-gfs-1.0.json > current-wind-surface-level-gfs-1.0.json
+#pwd
+#ls -a -o
